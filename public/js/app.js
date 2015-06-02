869ed3ba8dfce6bbf9f44c7350c011ee013caf1f
Matthew Smart <mdsmart36@gmail.com>
2015-06-01 21:05:54 -0500

    converted forms to jade

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,24 @@ angular.module('myApp', [
 ]).
 config(function ($routeProvider, $locationProvider) {
   $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
+    when('/', {
+      templateUrl: 'partials/login',
       controller: 'MyCtrl1'
     }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
+    when('/register', {
+      templateUrl: 'partials/register',
       controller: 'MyCtrl2'
     }).
+    when('/tasklist', {
+      templateUrl: 'partials/tasklist',
+      controller: 'MyCtrl3'
+    }).
+    when('/newtask', {
+      templateUrl: 'partials/newtask',
+      controller: 'MyCtrl4'
+    }).
     otherwise({
-      redirectTo: '/view1'
+      redirectTo: '/'
     });
 
   $locationProvider.html5Mode(true);
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,6 +3,7 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
+
   controller('AppCtrl', function ($scope, $http) {
 
     $http({
@@ -17,11 +18,23 @@ angular.module('myApp.controllers', []).
     });
 
   }).
+
   controller('MyCtrl1', function ($scope) {
     // write Ctrl here
 
   }).
+
   controller('MyCtrl2', function ($scope) {
+    // write Ctrl here
+
+  }).
+
+  controller('MyCtrl3', function ($scope) {
+    // write Ctrl here
+
+  }).
+
+  controller('MyCtrl4', function ($scope) {
     // write Ctrl here
 
   });
